bb93962fd3f4a396a4ba251a8fe89cccb9a28880
Fania Everitt <me@fania.eu>
2022-02-16 13:27:00 +0000

    randomise colours on refresh

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-console.log("hello world")
+const col = randomColor({luminosity: 'bright', count: 27});
 
-let col = randomColor({luminosity: 'bright', count: 27});
+const texts = [
+  `.n1  { background: ${col[0]};  }`,
+  `.n2  { background: ${col[1]};  }`,
+  `.n3  { background: ${col[2]};  }`,
+  `.n5  { background: ${col[3]};  }`,
+  `.n7  { background: ${col[4]};  }`,
+  `.n11 { background: ${col[5]};  }`,
+  `.n13 { background: ${col[6]};  }`,
+  `.n17 { background: ${col[7]};  }`,
+  `.n19 { background: ${col[8]};  }`,
+  `.n23 { background: ${col[9]};  }`,
+  `.n23 { background: ${col[10]}; }`,
+  `.n29 { background: ${col[11]}; }`,
+  `.n31 { background: ${col[12]}; }`,
+  `.n37 { background: ${col[13]}; }`,
+  `.n41 { background: ${col[14]}; }`,
+  `.n43 { background: ${col[15]}; }`,
+  `.n47 { background: ${col[16]}; }`,
+  `.n53 { background: ${col[17]}; }`,
+  `.n59 { background: ${col[18]}; }`,
+  `.n61 { background: ${col[19]}; }`,
+  `.n67 { background: ${col[20]}; }`,
+  `.n71 { background: ${col[21]}; }`,
+  `.n73 { background: ${col[22]}; }`,
+  `.n79 { background: ${col[23]}; }`,
+  `.n83 { background: ${col[24]}; }`,
+  `.n89 { background: ${col[25]}; }`,
+  `.n97 { background: ${col[26]}; }`];
 
-console.log(col);
+texts.forEach(text => {
+  console.log(text);
 
-// let classes = document.querySelectorAll("[class^='n']")
-// console.log(classes);
+  const sheet = document.styleSheets[0];
+  const [...rules] = sheet.cssRules;
+  sheet.insertRule(text, sheet.cssRules.length);
 
-// let unique = [...new Set([...classes])];
-// console.log(unique);
-
-let core = document.getElementById("core");
-console.log(core);
-console.log(core.children);
-let classes = document.querySelectorAll("core");
-console.log(classes);
-
-console.dir(core)
-
-for (let c in core) {
-  console.log(c.children);
-}
+});
